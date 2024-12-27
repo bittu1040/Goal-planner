873ba9b9bda08e4882a0d99ece9a8f51a2746ca5
Bittu <bittu1040@gmail.com>
2024-12-27 23:50:11 +0530

    updated timeline component

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class TimelineComponent {
 
+  goals: any = [
+    // { title: 'Goal 1', description: 'Description of Goal 1', date: 'Date 1' },
+    // { title: 'Goal 2', description: 'Description of Goal 2', date: 'Date 2' },
+    // { title: 'Goal 3', description: 'Description of Goal 3', date: 'Date 3' },
+    // { title: 'Goal 4', description: 'Description of Goal 4', date: 'Date 4' },
+    // { title: 'Goal 5', description: 'Description of Goal 5', date: 'Date 5' },
+    // { title: 'Goal 6', description: 'Description of Goal 6', date: 'Date 6' }
+  ];
+Math= Math;
 }
